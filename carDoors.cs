5091de5780ccbbf774f40343799445dbc1115d1e
SumanDeyDevOps <L00188359@atu.ie>
2024-10-15 21:01:29 +0100

    feature/123456-adding_car_doors_test

---
+++ carDoors.cs
@@ -0,0 +1,33 @@
+public class CarDoors
+{
+    public int rightSideDoors {get; set; }
+    public int leftSideDoors {get; set; }
+}
+
+public class Test
+{
+    private void carDoors(CarDoors car)
+    {
+        car.leftSideDoors = "2";
+        car.rightSideDoors = "2";
+    }
+
+        public void Test()
+        {
+            List<CarDoors> doors = new List<CarDoors>();
+            /// polulate some test data
+            for(int i = 0;i<10;i++)
+                doors.Add(new CarDoors());
+
+                // update the list using the UpdateCar method
+                doors.ForEach(carDoors);
+ 
+                // check the result
+            foreach (CarDoors car in doors)
+            {
+            Debug.WriteLine(car.leftSideDoors);
+            Debug.WriteLine(car.rightSideDoors.ToString());
+            }
+}
+}
+
